f282559f2d309d1fa07b0a880bd01cdd327114d9
Andrew Gunderman <agunderman10@gmail.com>
2019-04-16 06:24:10 -0400

    Moved all items to individual folders

---
+++ TDD_Course/UpdateableSpin/UpdateableSpinClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDD_Course
+{
+    public class UpdateableSpin
+    {
+        private readonly object lockObj = new object();
+        private bool shouldWait = true;
+        private long executionStartingTime;
+
+        public bool Wait(TimeSpan timeout, int spinDuration = 0)
+        {
+            UpdateTimeout();
+
+            while (true)
+            {
+                lock (lockObj)
+                {
+                    if (!shouldWait)
+                    {
+                        return true;
+                    }
+                    if (DateTime.UtcNow.Ticks - executionStartingTime > timeout.Ticks)
+                    {
+                        return false;
+                    }
+                }
+                Thread.Sleep(spinDuration);
+            }
+
+
+            return false;
+        }
+
+        public void Set()
+        {
+            lock (lockObj)
+            {
+                shouldWait = false;
+            }
+        }
+
+        public void UpdateTimeout()
+        {
+            lock (lockObj)
+            {
+                executionStartingTime = DateTime.UtcNow.Ticks;
+            }
+        }
+    }
+}
